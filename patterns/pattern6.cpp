0ab3b7a7889e3abf642c49cc7072b9e0817e4ea1
jaiskid <neeraj001jaiswal@gmail.com>
2019-07-16 18:22:42 +0530

    codeupdate

---
+++ patterns/pattern6.cpp
@@ -0,0 +1,18 @@
+//author name Neeraj jaiswal
+//date 16 july 2019
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+	int n;
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=n-i;j++){
+			cout<<" ";
+		}
+		for (int k=1;k<=2*i-1;k++){
+			cout<<"*";
+		}
+	}
+	cout<<endl;
+}
