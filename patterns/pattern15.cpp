9f48b310b1c8ab540774c09bb7b62c37b86e7d11
jaiskid <neeraj001jaiswal@gmail.com>
2019-07-16 23:04:40 +0530

    codeupdate

---
+++ patterns/pattern15.cpp
@@ -0,0 +1,35 @@
+//author name Neeraj jaiswal
+//date 16 july 2019
+/*
+*    * 
+*  * 
+** 
+*  * 
+*    * 
+*/
+#include <iostream>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	int k=(n+1)/2;
+	for(int i=1;i<=n;i++){
+		if(i<=k){
+			cout<<"*";
+			for(int j=1;j<=((n+1)-(2*i));j++){
+				cout<<" ";
+			}
+			cout<<"*";
+			cout<<" "<<endl;
+		}
+		else{
+			cout<<"*";
+			for(int j=1;j<=((2*i)-(n+1));j++)
+			{
+				cout<<" ";
+			}
+			cout<<"*";
+			cout<<" "<<endl;
+		}
+	}
+}
