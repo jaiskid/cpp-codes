0fe86effa5383277d22ec47415300ee63b3deb00
jaiskid <neeraj001jaiswal@gmail.com>
2019-07-17 23:11:49 +0530

    codeupdate

---
+++ patterns/pattern22.cpp
@@ -0,0 +1,26 @@
+//author name Neeraj jaiswal
+//date 17 july 
+/*
+1
+11
+111
+1001
+11111
+*/
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=i;j++){
+			if(i==j||j==1){
+				cout<<1;
+			}
+			else{
+				(i%2==0)?cout<<0:cout<<1;
+			}
+		}
+		cout<<endl;
+	}
+}
