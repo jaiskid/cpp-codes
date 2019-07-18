ac630c617c12d473c4cdc3c94cf05e0fe216125f
jaiskid <neeraj001jaiswal@gmail.com>
2019-07-18 16:04:10 +0530

    codeupdate

--- patterns/pattern21.cpp
+++ patterns/pattern21.cpp
@@ -1,11 +1,11 @@
 //author name Neeraj jaiswal
 //date 17 july 2019
 /*
-* ***
-* *  
-*****
-  * *
-*** *
+*   * * * 
+*   *     
+* * * * * 
+    *   * 
+* * *   * 
 */
 #include <iostream>
 using namespace std;
@@ -16,10 +16,10 @@ int main(){
 	for(int i=1;i<=n;i++){
 		for(int j=1;j<=n;j++){
 			if((i==1&&j>=mid)||(j==1&&i<=mid)||(j==n&&i>=mid)||(i==n&&j<=mid)||(i==mid||j==mid)){
-				cout<<"*";
+				cout<<"* ";
 			}
 			else{
-				cout<<" ";
+				cout<<"  ";
 			}
 		}
 		cout<<endl;
